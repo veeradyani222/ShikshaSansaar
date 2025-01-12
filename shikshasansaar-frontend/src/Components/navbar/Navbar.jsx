ab3cff23c9fea277a317d61bc4380123efb74588
HimanshuVerma765 <himanshuverma4765@gmail.com>
2025-01-12 13:41:45 +0530

    tried to create dropdown but not succeed
    i tried to build a custom dropdown but couldn't create

--- shikshasansaar-frontend/src/Components/navbar/Navbar.jsx
+++ shikshasansaar-frontend/src/Components/navbar/Navbar.jsx
@@ -80,7 +80,7 @@ const Navbar = () => {
   return (
     <div className='main-nav'>
       {/* Logout Confirmation Modal */}
-       {isLogoutModalOpen && (
+      {isLogoutModalOpen && (
         <div className="logout-modal">
           <div className="logout-modal-content">
             <h3>Are you sure you want to log out?</h3>
@@ -88,7 +88,7 @@ const Navbar = () => {
             <button onClick={() => setIsLogoutModalOpen(false)}>Cancel</button>
           </div>
         </div>
-      )} 
+      )}
 
       <nav>
         <div className="nav-left">
@@ -101,140 +101,140 @@ const Navbar = () => {
           </div>
         </div>
 
-<div className={`nav-content ${isMenuOpen ? 'show-menu' : ''}`}>
-  <li className={menu === 'Home' ? 'highlighted' : 'normal-nav'} onClick={() => { setMenu('Home'); handleMenuClick('/'); }}>
-    <Link to="/">Home</Link>
-  </li>
-  <li className={menu === 'About' ? 'highlighted' : 'normal-nav'} onClick={() => { setMenu('About'); handleMenuClick('/About'); }}>
-    <Link to="/About">About Us</Link>
-  </li>
-  <li className={menu === 'Courses' ? 'highlighted' : 'normal-nav'} onClick={() => { setMenu('Courses'); handleMenuClick('/Courses'); }}>
-    <Link to="/Courses">Courses</Link>
-  </li>
-  <li className={menu === 'Categories' ? 'highlighted' : 'normal-nav'} onClick={() => { setMenu('Categories'); handleMenuClick('/Categories'); }}>
-    <Link to="/Categories">Categories</Link>
-  </li>
-  <li onClick={toggleHelpDropdown} className='normal-nav'>
-    Help
-    <ul className={`dropdown ${isHelpDropdownOpen ? 'show-dropdown' : ''}`}>
-      <li className={menu === 'ContactUs' ? 'highlighted' : 'normal-nav1'} onClick={() => { setMenu('ContactUs'); handleHelpLinkClick('/ContactUs'); }}>
-        <Link to="/ContactUs">Contact Us</Link>
-      </li>
-      <li className={menu === 'FAQ' ? 'highlighted' : 'normal-nav1'} onClick={() => { setMenu('FAQ'); handleHelpLinkClick('/FAQ'); }}>
-        <Link to="/FAQ">FAQ</Link>
-      </li>
-      <li className={menu === 'Support' ? 'highlighted' : 'normal-nav1'} onClick={() => { setMenu('Support'); handleHelpLinkClick('/query'); }}>
-        <Link to="/query">Support</Link>
-      </li>
-    </ul>
-  </li>
-  <li onClick={togglePolicyDropdown} className='normal-nav'>
-   Our Policies
-    <ul className={`dropdown ${isPolicyDropdownOpen ? 'show-dropdown' : ''}`}>
-      <li className={menu === 'Terms' ? 'highlighted' : 'normal-nav1'} onClick={() => { setMenu('Terms'); handleHelpLinkClick('/Terms'); }}>
-        <Link to="/Terms">Terms of Use</Link>
-      </li>
-      <li className={menu === 'PrivacyPolicy' ? 'highlighted' : 'normal-nav1'} onClick={() => { setMenu('PrivacyPolicy'); handleHelpLinkClick('/PrivacyPolicy'); }}>
-        <Link to="/PrivacyPolicy">Privacy Policy</Link>
-      </li>
-      <li className={menu === 'RefundPolicy' ? 'highlighted' : 'normal-nav1'} onClick={() => { setMenu('RefundPolicy'); handleHelpLinkClick('/RefundPolicy'); }}>
-        <Link to="/RefundPolicy">Refund Policy</Link>
-      </li>
-      <li className={menu === 'ExchangePolicy' ? 'highlighted' : 'normal-nav1'} onClick={() => { setMenu('ExchangePolicy'); handleHelpLinkClick('/ExchangePolicy'); }}>
-        <Link to="/ExchangePolicy">Exchange Policy</Link>
-      </li>
-    </ul>
-  </li>
-  <li className={menu === 'GiveAReview' ? 'highlighted' : 'normal-nav'} onClick={() => { setMenu('GiveAReview'); handleMenuClick('/GiveAReview'); }}>
-    <Link to="/GiveAReview">Give A Review</Link>
-  </li>
-  <li className={menu === 'OfflineOrders' ? 'highlighted' : 'normal-nav'} onClick={() => { setMenu('OfflineOrders'); handleMenuClick('/OfflineOrders'); }}>
-    <Link to="/OfflineOrders">Offline Orders</Link>
-  </li>
-  <li className={menu === 'blog' ? 'highlighted' : 'normal-nav'} onClick={() => { setMenu('blog'); handleMenuClick('/blog'); }}>
-    <Link to="/blog">Blog</Link>
-  </li>
-  <li className={menu === 'faculties' ? 'highlighted' : 'normal-nav'} onClick={() => { setMenu('faculties'); handleMenuClick('/faculties'); }}>
-    <Link to="/blog">Faculties</Link>
-  </li>
-</div>
+        <div className={`nav-content ${isMenuOpen ? 'show-menu' : ''}`}>
+          <li className={menu === 'Home' ? 'highlighted' : 'normal-nav'} onClick={() => { setMenu('Home'); handleMenuClick('/'); }}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={menu === 'About' ? 'highlighted' : 'normal-nav'} onClick={() => { setMenu('About'); handleMenuClick('/About'); }}>
+            <Link to="/About">About Us</Link>
+          </li>
+          <li className={menu === 'Courses' ? 'highlighted' : 'normal-nav'} onClick={() => { setMenu('Courses'); handleMenuClick('/Courses'); }}>
+            <Link to="/Courses">Courses</Link>
+          </li>
+          <li className={menu === 'Categories' ? 'highlighted' : 'normal-nav'} onClick={() => { setMenu('Categories'); handleMenuClick('/Categories'); }}>
+            <Link to="/Categories">Categories</Link>
+          </li>
+          <li onClick={toggleHelpDropdown} className='normal-nav'>
+            Help
+            <ul className={`dropdown ${isHelpDropdownOpen ? 'show-dropdown' : ''}`}>
+              <li className={menu === 'ContactUs' ? 'highlighted' : 'normal-nav1'} onClick={() => { setMenu('ContactUs'); handleHelpLinkClick('/ContactUs'); }}>
+                <Link to="/ContactUs">Contact Us</Link>
+              </li>
+              <li className={menu === 'FAQ' ? 'highlighted' : 'normal-nav1'} onClick={() => { setMenu('FAQ'); handleHelpLinkClick('/FAQ'); }}>
+                <Link to="/FAQ">FAQ</Link>
+              </li>
+              <li className={menu === 'Support' ? 'highlighted' : 'normal-nav1'} onClick={() => { setMenu('Support'); handleHelpLinkClick('/query'); }}>
+                <Link to="/query">Support</Link>
+              </li>
+            </ul>
+          </li>
+          <li onClick={togglePolicyDropdown} className='normal-nav'>
+            Our Policies
+            <ul className={`dropdown ${isPolicyDropdownOpen ? 'show-dropdown' : ''}`}>
+              <li className={menu === 'Terms' ? 'highlighted' : 'normal-nav1'} onClick={() => { setMenu('Terms'); handleHelpLinkClick('/Terms'); }}>
+                <Link to="/Terms">Terms of Use</Link>
+              </li>
+              <li className={menu === 'PrivacyPolicy' ? 'highlighted' : 'normal-nav1'} onClick={() => { setMenu('PrivacyPolicy'); handleHelpLinkClick('/PrivacyPolicy'); }}>
+                <Link to="/PrivacyPolicy">Privacy Policy</Link>
+              </li>
+              <li className={menu === 'RefundPolicy' ? 'highlighted' : 'normal-nav1'} onClick={() => { setMenu('RefundPolicy'); handleHelpLinkClick('/RefundPolicy'); }}>
+                <Link to="/RefundPolicy">Refund Policy</Link>
+              </li>
+              <li className={menu === 'ExchangePolicy' ? 'highlighted' : 'normal-nav1'} onClick={() => { setMenu('ExchangePolicy'); handleHelpLinkClick('/ExchangePolicy'); }}>
+                <Link to="/ExchangePolicy">Exchange Policy</Link>
+              </li>
+            </ul>
+          </li>
+          <li className={menu === 'GiveAReview' ? 'highlighted' : 'normal-nav'} onClick={() => { setMenu('GiveAReview'); handleMenuClick('/GiveAReview'); }}>
+            <Link to="/GiveAReview">Give A Review</Link>
+          </li>
+          <li className={menu === 'OfflineOrders' ? 'highlighted' : 'normal-nav'} onClick={() => { setMenu('OfflineOrders'); handleMenuClick('/OfflineOrders'); }}>
+            <Link to="/OfflineOrders">Offline Orders</Link>
+          </li>
+          <li className={menu === 'blog' ? 'highlighted' : 'normal-nav'} onClick={() => { setMenu('blog'); handleMenuClick('/blog'); }}>
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li className={menu === 'faculties' ? 'highlighted' : 'normal-nav'} onClick={() => { setMenu('faculties'); handleMenuClick('/faculties'); }}>
+            <Link to="/blog">Faculties</Link>
+          </li>
+        </div>
       </nav>
 
 
       <div
-  className="mobile-login-profile-1"
-  style={{
-    display: "flex",
-    flexDirection: "column",
-  }}
->
+        className="mobile-login-profile-1"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
 
 
-  {/* Logo */}
-  <div className="mobile-login-profile">
+        {/* Logo */}
+        <div className="mobile-login-profile">
 
-  <div className="something">
-    <div className="logo" onClick={() => navigate('/')}>
-      <img className="logo-img" src={logo} alt="Logo" />
+          <div className="something">
+            <div className="logo" onClick={() => navigate('/')}>
+              <img className="logo-img" src={logo} alt="Logo" />
+            </div>
+          </div>
+
+          {/* Authentication Options */}
+          {localStorage.getItem('auth-token') ? (
+            <li className="login" onClick={() => setIsLogoutModalOpen(true)}>Logout</li>
+          ) : (
+            <Link to="/login">
+              <li className="login">Login</li>
+            </Link>
+          )}
+
+          {/* Profile Section */}
+          <div className="cart profile" onClick={() => checkAuthentication('/profile')}>
+            <li className="profile">
+              <img src={user_icon} alt="User" />
+            </li>
+          </div>
+
+          {/* Wishlist Section */}
+          <div className="cart" onClick={() => checkAuthentication('/wishlist')}>
+            <div className="cart-wishlist-number">{totalWishlistItems}</div>
+            <li className="wishlist">
+              <img src={heart_icon} alt="Wishlist" />
+            </li>
+          </div>
+
+          {/* Shopping Cart Section */}
+          <div className="cart shopcart" onClick={() => checkAuthentication('/cart')}>
+            <div className="cart-cart-number">{totalCartItems}</div>
+            <li className="cart shopcart">
+              <img src={cart_icon} alt="Cart" />
+            </li>
+          </div>
+
+          {/* Search Icon */}
+          <div className="search-icon" onClick={() => setIsSearchVisible(!isSearchVisible)}>
+            <img src={search_icon} alt="search" />
+          </div>
+
+          <div className="hamburger-menu" onClick={toggleMenu}>
+            {isMenuOpen ? <img src={remove_icon} alt="search" /> : <img src={hamburger_icon} alt="search" />}
+          </div>
+        </div>
+        {/* Search Bar (conditionally displayed) */}
+        <div className="visible-search">{isSearchVisible && (
+          <div className="search-bar1" ref={searchBarRef}>
+            <Search onProductSelect={() => setIsSearchVisible(false)} />
+          </div>
+        )}</div>
+
+      </div>
+
+
+
+
     </div>
-  </div>
-
-  {/* Authentication Options */}
-  {localStorage.getItem('auth-token') ? (
-    <li className="login" onClick={() => setIsLogoutModalOpen(true)}>Logout</li>
-  ) : (
-    <Link to="/login">
-      <li className="login">Login</li>
-    </Link>
-  )}
-
-  {/* Profile Section */}
-  <div className="cart profile" onClick={() => checkAuthentication('/profile')}>
-    <li className="profile">
-      <img src={user_icon} alt="User" />
-    </li>
-  </div>
-
-  {/* Wishlist Section */}
-  <div className="cart" onClick={() => checkAuthentication('/wishlist')}>
-    <div className="cart-wishlist-number">{totalWishlistItems}</div>
-    <li className="wishlist">
-      <img src={heart_icon} alt="Wishlist" />
-    </li>
-  </div>
-
-  {/* Shopping Cart Section */}
-  <div className="cart shopcart" onClick={() => checkAuthentication('/cart')}>
-    <div className="cart-cart-number">{totalCartItems}</div>
-    <li className="cart shopcart">
-      <img src={cart_icon} alt="Cart" />
-    </li>
-  </div>
-
-  {/* Search Icon */}
-  <div className="search-icon" onClick={() => setIsSearchVisible(!isSearchVisible)}>
-    <img src={search_icon} alt="search" />
-  </div>
-
-  <div className="hamburger-menu" onClick={toggleMenu}>
-    {isMenuOpen ? <img src={remove_icon} alt="search" /> : <img src={hamburger_icon} alt="search" /> }
-  </div>
-  </div>
-  {/* Search Bar (conditionally displayed) */}
-<div className="visible-search">{isSearchVisible && (
-  <div className="search-bar1" ref={searchBarRef}>
-    <Search onProductSelect={() => setIsSearchVisible(false)} />
-  </div>
-)}</div>
-  
-</div>
 
 
-
-
-    </div>
-
-    
   );
 };
 
